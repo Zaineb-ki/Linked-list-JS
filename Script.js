76a3469d544a62a74e7d3a9e2118cfb47edca602
Zaineb-ki <kossorizaineb@gmail.com>
2020-01-21 14:10:37 +0100

    last commit

--- Script.js
+++ Script.js
@@ -1,129 +1,177 @@
-class Node{
-    constructor(data,next = null){
-        this.data = data ;
-        this.next = next;
+// Construct Single Node
+class Node {
+    constructor(data, next = null) {
+      this.data = data;
+      this.next = next;
     }
-}
-
-class LinkedList{
-    constructor(){
-        this.head = null ;
-        this.size = 0;
+  }
+  
+  // Create/Get/Remove Nodes From Linked List
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.size = 0;
     }
-// Insert first node
-insertFirst(data){
-    this.head = new Node(data, this.head);
-    this.size++;
-}
-
-// Insert last node
-insertlast(data){
-    let node = new Node(data);
-    let current;
-    
-    if(!this.head){
+  
+    // Insert first node
+    insertFirst(data) {
+      this.head = new Node(data, this.head);
+      this.size++;
+    }
+  
+    // Insert last node
+    insertLast(data) {
+      let node = new Node(data);
+      let current;
+  
+      // If empty, make head
+      if (!this.head) {
         this.head = node;
-    }
-    else{
+      } else {
         current = this.head;
+  
         while (current.next) {
-            current = current.next;
+          current = current.next;
         }
+  
         current.next = node;
+      }
+  
+      this.size++;
     }
-    this.size++;
-}
-
-// Insert at index
-inserAt(data, index){
-    if(index > 0 && index > this.size){
+  
+    // Insert at index
+    insertAt(data, index) {
+      //  If index is out of range
+      if (index > 0 && index > this.size) {
         return;
-    }
-    if (index ===0) {
-        this.head = newNode(data, this.head);
+      }
+  
+      // If first index
+      if (index === 0) {
+        this.insertFirst(data);
         return;
+      }
+  
+      const node = new Node(data);
+      let current, previous;
+  
+      // Set current to first
+      current = this.head;
+      let count = 0;
+  
+      while (count < index) {
+        previous = current; // Node before index
+        count++;
+        current = current.next; // Node after index
+      }
+  
+      node.next = current;
+      previous.next = node;
+  
+      this.size++;
     }
-    const node = new Node(data);
-    let current , previous;
-
-    current = this.head;
-    let count = 0;
-
-    while (count < index) {
-        previous = current;
+  
+    // Get at index
+    getAt(index) {
+      let current = this.head;
+      let count = 0;
+  
+      while (current) {
+        if (count == index) {
+          document.getElementById("demo1").innerHTML = current.data;
+          
+        }
         count++;
         current = current.next;
+      }
+  
+      return null;
     }
-
-    node.next = current;
-    previous.next = node;
-
-    this.size++;
-}
-
-// Get at index
-getAt(index){
-    let current = this.head;
-    let count = 0;
-
-    while (current) {
-        if (count == index) {
-           console.log(current.data); 
+  
+    // Remove at index
+    removeAt(index) {
+      if (index > 0 && index > this.size) {
+        return;
+      }
+  
+      let current = this.head;
+      let previous;
+      let count = 0;
+  
+      // Remove first
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        while (count < index) {
+          count++;
+          previous = current;
+          current = current.next;
         }
-        count ++;
+  
+        previous.next = current.next;
+      }
+  
+      this.size--;
+    }
+  
+    // Clear list
+    clearList() {
+      this.head = null;
+      this.size = 0;
+    }
+  
+    // Print list data
+    printListData() {
+      let current = this.head;
+       var listName = []
+      while (current) {
+        listName.push(current.data);
         current = current.next;
+      }
+      return listName;
     }
-    return null;
-}
-
-// Remove at index
-removeAt(index){
-    if (index > 0 && index > this.size) {
-        return
-    }
-    let current = this.head;
-    let previous;
-    let count = 0
-    //Remove the first node
-     if (index ===0) {
-         this.head = current.next;
-     } else {
-         while (count < index) {
-             count ++;
-             previous = current;
-             current = current.next;
-         }
-
-         previous.next = current.next;
-     }
-     this.size--;
-}
-
-// Clear list
-clearList(){
-    this.head = null;
-    this.size = 0;
-}
-
-// Print list data
-    printListData(){
-            let current = this.head;
-    }
-    while (current) {
-        console.log(current.data);
-        current = current.next;
-        
-    }
-
-}
-
-const ll = new LinkedList();
-ll.insertFirst(100);
-ll.insertFirst(200);
-ll.insertFirst(300);
-ll.insertlast(400);
-// ll.inserAt(500, 3)
-
-// ll.printListData();
-
-// ll.getAt(10);
+  }
+  
+  const ll = new LinkedList();
+  
+  
+  function addFirst(){
+      var nam = document.getElementById('Nom').value;
+      ll.insertFirst(nam);
+     
+  }
+  function addlast(){
+      var nam = document.getElementById('Nom').value;
+      ll.insertLast(nam);
+  }
+  function insertAt(){
+      var nam = document.getElementById('Nom').value;
+      var index = document.getElementById('indice').value;
+      ll.insertAt(nam,index);
+  }
+  function affichAt() {
+              var index = document.getElementById('indice').value;
+              ll.getAt(index);
+          }
+          function affichAll(){
+              var names, text, fLen, i;
+              names = ll.printListData();
+          fLen = names.length;
+  
+          text = "<ol>";
+          for (i = 0; i < fLen; i++) {
+          text += "<li>" + names[i] + "</li>";
+          }
+          text += "</ol>";
+  
+  document.getElementById("demo").innerHTML = text;
+  }
+  function supAt(){
+      var index = document.getElementById("indice").value;
+      ll.removeAt(index)
+  }
+  function supAll(){
+      ll.clearList();
+  } 
+   
